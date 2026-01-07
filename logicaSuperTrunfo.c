4f96ac88d1aad28c3c2b6e6a29d52b3185c3dfc5
caiocardosonunes123-web <caiocardosonunes123@gmail.com>
2026-01-07 06:36:24 +0000

    idade

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
-int main()
-printf("logic super trunfo\n.");
+int main (){
+    int idade;
+    printf ("digite a sua idade: \n");
+    scanf ("%d", idade);
+
+    if (idade < 12 ){{
+        printf ("digite a sua idade\n");
+
+        if (idade >= && idade < 18){
+            pritnf ("voce e apenas um adolescente\n");
+
+            else if (idade >=18 && < 60){
+                printf ("voce e um adulto\n");
+                else{
+                    printf("voce e um idoso\n");
+
+                }
+            }
+
+        }
+    }
+ 
 
 
 
